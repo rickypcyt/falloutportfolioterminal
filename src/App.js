ce202b4de9164ecb4c0870d6a7fb798382db6743
rickypcyt <rickypcyt@gmail.com>
2024-04-01 00:40:40 +0200

    V1

--- src/App.js
+++ src/App.js
@@ -1,39 +1,46 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './App.css'; // Asegúrate de que sea la ruta correcta al archivo CSS
-import imagenLocal from './bg.png'; // Ruta relativa a la imagen local
+import imagenLocal from './bg2.png'; // Ruta relativa a la imagen local
 
-class ImageComponent extends React.Component {
-  componentDidMount() {
-    // Función para dibujar en el canvas
-    const draw = () => {
-      const canvas = this.refs.canvas; // Usamos refs para acceder al elemento canvas
+const ImageComponent = () => {
+  const canvasRef = useRef(null);
+  const [canvasPosition, setCanvasPosition] = useState({ top: '104px', left: '592px' });
+  useEffect(() => {
+    const updateCanvasPosition = () => {
+      const canvas = canvasRef.current;
       if (canvas) {
+        const windowWidth = window.innerWidth;
+        const windowHeight = window.innerHeight;
+        canvas.width = windowWidth; // Ajusta el ancho del canvas al ancho de la ventana
+        canvas.height = windowHeight; // Ajusta la altura del canvas a la altura de la ventana
         const ctx = canvas.getContext('2d');
-        // Limpiar el canvas
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        // Obtener las coordenadas del centro del canvas
-        const centerX = canvas.width / 2;
-        const centerY = canvas.height / 2;
-        // Dibujar un rectángulo de ejemplo en el centro del canvas
-        ctx.fillStyle = '#FF0000';
-        ctx.fillRect(centerX + 30 , centerY + 50, 175, 175); // El rectángulo se posiciona en el centro y se ajusta su tamaño
-        // Ejemplo de ejecución de código
-        console.log('¡Código ejecutado dentro del canvas!');
+        ctx.fillStyle = '#5a585f'; // Establece el color de fondo en negro
+        ctx.fillRect(0, 0, windowWidth, windowHeight); // Dibuja un rectángulo negro que cubre toda la pantalla
       }
     };
 
-    // Llamar a la función draw al cargar el componente
-    draw();
-  }
+    // Llama a la función para inicializar la posición y el color del canvas
+    updateCanvasPosition();
 
-  render() {
-    return (
-      <div className="image-container">
-        <img src={imagenLocal} alt="Descripción de la imagen" />
-        <canvas className="canvas-overlay" ref="canvas"></canvas>
-      </div>
-    );
-  }
-}
+    // Agrega un event listener para detectar cambios en el tamaño de la ventana
+    window.addEventListener('resize', updateCanvasPosition);
+
+    // Limpia el event listener cuando el componente se desmonta
+    return () => {
+      window.removeEventListener('resize', updateCanvasPosition);
+    };
+  }, []); // El array vacío indica que este efecto se ejecuta solo una vez, al montar el componente
+
+  return (
+    <div className="image-container">
+      <img src={imagenLocal} alt="Descripción de la imagen" />
+      <canvas
+        className="canvas-overlay"
+        style={{ top: canvasPosition.top, left: canvasPosition.left, position: 'absolute' }}
+        ref={canvasRef}>
+      </canvas>
+    </div>
+  );
+};
 
 export default ImageComponent;
