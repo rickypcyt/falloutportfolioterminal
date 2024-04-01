3c16ba74a00c6b4eea8083c9c67f19c155a79933
rickypcyt <rickypcyt@gmail.com>
2024-04-01 14:36:56 +0200

    Buena

--- src/App.js
+++ src/App.js
@@ -1,73 +1,16 @@
-import React, { useRef, useEffect, useState } from 'react';
-import * as THREE from 'three';
-import bgImage from './bg2.png'; // Importa la imagen aquí
+import React from 'react';
+import terminalImage from './bg3nobg.png'; // Importa la imagen aquí
 import Terminal from './terminal';
-import PP from "./pip-boy-fallout-3-concept/source/low.fbx"
 
-const ImageThreeJs = () => {
-  const canvasRef = useRef(null);
-  const containerRef = useRef(null); // Referencia al contenedor HTML para el terminal
-
-  const [windowSize, setWindowSize] = useState({
-    width: window.innerWidth,
-    height: window.innerHeight,
-  });
-
-  useEffect(() => {
-    const handleResize = () => {
-      setWindowSize({
-        width: window.innerWidth,
-        height: window.innerHeight,
-      });
-    };
-
-    window.addEventListener('resize', handleResize);
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
-
-  useEffect(() => {
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(75, windowSize.width / windowSize.height, 0.1, 1000);
-
-    const renderer = new THREE.WebGLRenderer({ canvas: canvasRef.current });
-    renderer.setSize(windowSize.width, windowSize.height);
-
-    const geometry = new THREE.PlaneGeometry(13, 10); // Cambia las dimensiones del plano para estirar la imagen
-    const loader = new THREE.TextureLoader();
-    const texture = loader.load(bgImage); // Utiliza la imagen importada
-    const material = new THREE.MeshBasicMaterial({ map: texture });
-    const plane = new THREE.Mesh(geometry, material);
-    scene.add(plane);
-
-    camera.position.z = 7;
-
-    const animate = () => {
-      requestAnimationFrame(animate);
-
-      renderer.render(scene, camera);
-    };
-
-    animate();
-
-    return () => {
-      renderer.dispose();
-    };
-  }, [windowSize]);
-
+const ImageComponent = () => {
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100vw', height: '99.62vh', maxWidth: '100%', maxHeight: '100%' }}>
-
-      <div ref={containerRef}>
-        <canvas ref={canvasRef} style={{ width: '100%', height: '100%' }} />
-      </div>
-      <div style={{ position: 'absolute', top: '37.5%', left: '53%', transform: 'translate(-50%, -50%)' }}>
+    <div style={{ position: 'relative', width: '99vw', height: '99.6vh', backgroundColor: 'black' }}>
+      <img src={terminalImage} alt="Terminal" style={{ width: '100%', height: '100%', objectFit: 'contain' }} />
+      <div style={{ position: 'absolute', top: '39%', left: '43.5%', transform: 'translate(-50%, -50%)' }}>
         <Terminal />
       </div>
     </div>
   );
 }
 
-export default ImageThreeJs;
+export default ImageComponent;
--- src/terminal.js
+++ src/terminal.js
@@ -1,9 +1,74 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './terminal.css';
 
 const Terminal = () => {
   const [inputValue, setInputValue] = useState('');
-  const [output, setOutput] = useState('Welcome to my portfolio terminal. Type "start" to initiate.');
+  const [outputBeforeRobco, setOutputBeforeRobco] = useState('');
+  const [outputAfterRobco, setOutputAfterRobco] = useState('');
+  const [messageIndex, setMessageIndex] = useState(0);
+  const [charIndex, setCharIndex] = useState(0);
+  const [showRobcoAscii, setShowRobcoAscii] = useState(false);
+  const [showInput, setShowInput] = useState(false);
+  const [showOutputAfterStart, setShowOutputAfterStart] = useState(true);
+
+  const messagesBeforeRobco = [
+    "Initializing boot...",
+    "Loading RobCo Unified OS...",
+    "64K RAM detected...",
+    "Launching Interface..."
+  ];
+
+  const robcoAsciiArt = [
+    "  _____       _      _____                   ",
+    " |  __ \\     | |    / ____|                  ",
+    " | |__) |___ | |__ | |      ___               ",
+    " |  _  // _ \\| '_ \\| |     / _ \\              ",
+    " | | \\ \\ (_) | |_) | |____| (_) |             ",
+    " |_|  \\_\\___/|_.__/ \\_____|\\___/          ",
+    "",
+  ];
+
+  const messagesAfterRobco = [
+    "==============================================",
+    "Personal Terminal 'Proto-Boy' Manufactured by RobCo",
+    "Type start to continue:"
+  ];
+
+  useEffect(() => {
+    const typingTimer = setInterval(() => {
+      if (messageIndex < messagesBeforeRobco.length) {
+        const currentMessage = messagesBeforeRobco[messageIndex];
+        if (charIndex < currentMessage.length) {
+          setOutputBeforeRobco(prevOutput => prevOutput + currentMessage.charAt(charIndex));
+          setCharIndex(prevIndex => prevIndex + 1);
+        } else {
+          setMessageIndex(prevIndex => prevIndex + 1);
+          setCharIndex(0);
+          setOutputBeforeRobco(prevOutput => prevOutput + '\n');
+        }
+      } else if (messageIndex === messagesBeforeRobco.length) {
+        setOutputBeforeRobco('');
+        setShowRobcoAscii(true);
+        setMessageIndex(prevIndex => prevIndex + 1);
+      } else if (messageIndex < messagesBeforeRobco.length + messagesAfterRobco.length) {
+        const currentMessage = messagesAfterRobco[messageIndex - messagesBeforeRobco.length];
+        if (currentMessage === "Type start to continue:") {
+          setShowInput(true);
+        }
+        if (charIndex < currentMessage.length) {
+          setOutputAfterRobco(prevOutput => prevOutput + currentMessage.charAt(charIndex));
+          setCharIndex(prevIndex => prevIndex + 1);
+        } else {
+          setMessageIndex(prevIndex => prevIndex + 1);
+          setCharIndex(0);
+          setOutputAfterRobco(prevOutput => prevOutput + '\n');
+        }
+      } else {
+        clearInterval(typingTimer);
+      }
+    }, 50);
+    return () => clearInterval(typingTimer);
+  }, [charIndex, messageIndex, messagesBeforeRobco, messagesAfterRobco, showRobcoAscii]);
 
   const handleInputChange = (event) => {
     setInputValue(event.target.value);
@@ -12,27 +77,56 @@ const Terminal = () => {
   const handleInputSubmit = (event) => {
     event.preventDefault();
     if (inputValue.trim() === 'start') {
-      setOutput('Portfolio initiated. You can now proceed with your commands.');
+      setOutputAfterRobco('');
+      setShowRobcoAscii(false);
+      setShowOutputAfterStart(false);
+      setInputValue('');
     } else {
-      setOutput(`Command '${inputValue}' not recognized.`);
+      setOutputAfterRobco(prevOutput => prevOutput + `Command '${inputValue}' not recognized.\n`);
+      setInputValue('');
     }
-    setInputValue('');
   };
 
   return (
-    <div className="terminal">
-      <div className="content">
-        <pre>{output}</pre>
-        <form onSubmit={handleInputSubmit}>
-          <span className="prompt">$ </span>
-          <input
-            type="text"
-            value={inputValue}
-            onChange={handleInputChange}
-            className="input"
-            autoFocus
-          />
-        </form>
+    <div className="terminal" style={{ 
+      backgroundColor: "#151614",
+      color: "lime",
+      fontFamily: "monospace",
+      borderRadius: "8px",
+      overflow: "hidden",
+      width: "415px",
+      height: "315px"
+    }}>
+      <div className="content" style={{
+        padding: "10px",
+        overflowY: "auto"
+      }}>
+        <pre>{outputBeforeRobco}</pre>
+        {showRobcoAscii && (
+          <pre>{robcoAsciiArt.join('\n')}</pre>
+        )}
+        <br />
+        {showOutputAfterStart && <pre>{outputAfterRobco}</pre>}
+        {showInput && (
+          <form onSubmit={handleInputSubmit}>
+            <span className="prompt">$ </span>
+            <input
+              type="text"
+              value={inputValue}
+              onChange={handleInputChange}
+              className="input"
+              style={{
+                background: "transparent",
+                border: "none",
+                color: "lime",
+                fontFamily: "monospace",
+                outline: "none",
+                width: "calc(100% - 30px)"
+              }}
+              autoFocus
+            />
+          </form>
+        )}
       </div>
     </div>
   );
